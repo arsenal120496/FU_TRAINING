f01076d655efda30f6713b008043d3bd59f17f30
khanh <trankhanh10ch@gmail.com>
2017-04-13 09:24:40 +0700

    Update login controller, location controller (mobile)

--- trunk/GST/src/main/java/com/gst/controller/LocationController.java
+++ trunk/GST/src/main/java/com/gst/controller/LocationController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +21,14 @@ public class LocationController {
 	private LocationService locationService;
 	
 	
-	@RequestMapping("/addLocation")
+	@RequestMapping(value = "/addLocation" , method = RequestMethod.POST)
 	public boolean addLocation(@RequestParam(value = "email", required = true) String  email, 
 							@RequestParam(value = "deviceName", required = true) String deviceName,
 							@RequestParam(value = "longtitude", required = true) String longtitude,
 							@RequestParam(value = "latitude", required = true) String latitude){
 		Date d = new Date();
 		SimpleDateFormat sdf =  new SimpleDateFormat();
-		String time = sdf.format(d);
+		String time = sdf.format(d);	
 		UserLocation u = new UserLocation(email,new Location(longtitude, latitude),deviceName, time);
 		locationService.save(u);
 		return true;
